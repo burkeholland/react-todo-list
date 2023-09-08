e33fde64213905cd79426b512756dc22ca585375
Harald Kirschner <hkirschner@microsoft.com>
2023-09-08 15:04:03 -0700

    Working tests

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+describe('App Component', () => {
+    test('Renders without crashing', () => {
+        const div = document.createElement('div');
+        const root = createRoot(div);
+        root.render(<App />);
+        root.unmount();
+    });
+});
+
